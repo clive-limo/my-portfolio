ed1b255d7733a180cec3a020a63d011f11d68b86
Clive Limo <clivelimo84@gmail.com>
2022-05-26 05:26:35 +0300

    [SITE-TRANSLATION] Initialize Site Translation

--- next.config.js
+++ next.config.js
@@ -10,8 +10,16 @@ module.exports = withBundleAnalyzer({
   poweredByHeader: false,
   trailingSlash: true,
   basePath: '',
-  // The starter code load resources from `public` folder with `router.basePath` in React components.
-  // So, the source code is "basePath-ready".
-  // You can remove `basePath` if you don't need it.
   reactStrictMode: true,
+});
+
+const nextTranslate = require('next-translate');
+
+module.exports = nextTranslate({
+  webpack: (config) => {
+    return config;
+  },
+  generateBuildId: () => {
+    return `${Date.now()}`;
+  },
 });
--- src/components/NavBar/components/DesktopNavBar/index.tsx
+++ src/components/NavBar/components/DesktopNavBar/index.tsx
@@ -1,10 +1,13 @@
 import { useRouter } from 'next/router';
 import type { FC } from 'react';
 
+import usePageTranslation from '@/hooks/usePageTranslation';
+
 import NavButton from './components/NavButton';
 
 const DesktopNavBar: FC = () => {
   const router = useRouter();
+  const { t } = usePageTranslation('nav', 'Nav');
 
   return (
     <header className="hidden md:flex ">
@@ -14,20 +17,20 @@ const DesktopNavBar: FC = () => {
         className="h-[100px] object-contain"
       />
       <span className="flex-[6]" />
-      <NavButton path="/" title="HOME" active={router.pathname === '/'} />
+      <NavButton path="/" title={t('home')} active={router.pathname === '/'} />
       <NavButton
         path="/about"
-        title="ABOUT"
+        title={t('about')}
         active={router.pathname === '/about'}
       />
       <NavButton
         path="/forHire"
-        title="HIRE ME"
+        title={t('experience')}
         active={router.pathname === '/forHire'}
       />
       <span className="flex-[1]" />
       <button className="self-center rounded-3xl bg-dark-secondary ">
-        <p className="px-10 py-3 text-white">Contact Me</p>
+        <p className="px-10 py-3 text-white">{t('contact')}</p>
       </button>
       <span className="flex-[.5]" />
     </header>
